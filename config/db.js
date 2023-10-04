0419ba76720159a3edc9f4eadbd1cbbcc941b2b6
Ömer Kahveci <omerkahveci53@hotmail.com>
2023-10-04 20:07:08 +0300

    mongo db connection

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 export const dbConnect = async () =>{
-
+    const connectString = process.env.NEXT_PUBLIC_MONGO_URI;
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(connectString);
 
         console.log("Database connected")
     } catch (error) {
